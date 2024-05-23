72c3ae5c3b3d22532ce1d5ec04ecf65506c16485
Victor Vinicius <victormafiavinicius@gmail.com>
2024-05-23 09:39:45 -0300

    Criando a context. Iniciando o banco de dados.

---
+++ WebApiCatalogo/Catalogo.Infrastucture/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiCatalogo.Catalogo.Core.Model;
+
+namespace WebApiCatalogo.Catalogo.Infrastucture.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { 
+    }
+
+    public DbSet<CategoriaModel>? Categorias { get; set; }
+    public DbSet<ProdutoModel>? Produto { get; set; }    
+}
+
