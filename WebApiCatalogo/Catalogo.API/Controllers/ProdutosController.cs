3244713246408a72a488bed6980364498ca10bd1
Victor Vinicius <victormafiavinicius@gmail.com>
2024-05-24 10:23:47 -0300

    Controller de produtos iniciado

---
+++ WebApiCatalogo/Catalogo.API/Controllers/ProdutosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiCatalogo.Catalogo.Infrastucture.Context;
+
+namespace WebApiCatalogo.Catalogo.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
