f669141f43ffbb24b59bad1f61991a0afa1f2162
Victor Vinicius <victormafiavinicius@gmail.com>
2024-05-24 10:56:32 -0300

    Pesquisando produtos pelo ID (GET id)

--- WebApiCatalogo/Catalogo.API/Controllers/ProdutosController.cs
+++ WebApiCatalogo/Catalogo.API/Controllers/ProdutosController.cs
@@ -26,5 +26,16 @@ namespace WebApiCatalogo.Catalogo.API.Controllers
             }
             return produtos;
         }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<ProdutoModel> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado.");
+            }
+            return produto;
+        }
     }
 }
