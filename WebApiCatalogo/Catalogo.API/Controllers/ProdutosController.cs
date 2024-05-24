7c221af1fe17c8be4f8476c23f921e61405794cb
Victor Vinicius <victormafiavinicius@gmail.com>
2024-05-24 10:48:30 -0300

    Listando todos os produtos (Get)

--- WebApiCatalogo/Catalogo.API/Controllers/ProdutosController.cs
+++ WebApiCatalogo/Catalogo.API/Controllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApiCatalogo.Catalogo.Core.Model;
 using WebApiCatalogo.Catalogo.Infrastucture.Context;
 
 namespace WebApiCatalogo.Catalogo.API.Controllers
@@ -13,6 +14,17 @@ namespace WebApiCatalogo.Catalogo.API.Controllers
         public ProdutosController(AppDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ProdutoModel>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados.");
+            }
+            return produtos;
         }
     }
 }
