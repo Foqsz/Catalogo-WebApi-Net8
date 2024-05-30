3cf14f64622f734de3dd6694e5ca7cd737bb52eb
Victor Vinicius <victormafiavinicius@gmail.com>
2024-05-30 10:55:50 -0300

    Métodos de validação

--- WebApiCatalogo/Catalogo.Core/Model/ProdutoModel.cs
+++ WebApiCatalogo/Catalogo.Core/Model/ProdutoModel.cs
@@ -6,21 +6,21 @@ using WebApiCatalogo.Catalogo.Application.Validations;
 namespace WebApiCatalogo.Catalogo.Core.Model;
 
 [Table("Produtos")]
-public class ProdutoModel
+public class ProdutoModel : IValidatableObject
 {
     [Key]
     public int ProdutoId { get; set; }
 
     [Required(ErrorMessage = "O nome é obrigatório")]
     [StringLength(40, ErrorMessage = "O Nome deve ter entre 5 e 20 caracteres.", MinimumLength = 5)]
-    [PrimeiraLetraMaiusculaAtributte]
+    //[PrimeiraLetraMaiusculaAtributte]
     public string Nome { get; set; }
 
     [Required]
     [StringLength(30, ErrorMessage = "A Descrição deve ter no máximo {1} caracteres.")]
     public string Descricao { get; set; }
 
-    [Required] 
+    [Required]
     [Range(1, 1000, ErrorMessage = "O Preço deve estar entre {1} e {1000}.")]
     public decimal Preco { get; set; }
 
@@ -33,6 +33,31 @@ public class ProdutoModel
 
     [JsonIgnore]
     public CategoriaModel? Categoria { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new //yield indica que o método ou o operador é um interador
+                    ValidationResult("A primeira letra do produto deve ser maiúscula.",
+                new[]
+                {nameof(this.Nome) }
+                );
+            }
+        }
+        if (this.Estoque <= 0)
+        {
+            yield return new //yield indica que o método ou o operador é um interador
+                ValidationResult("O estoque deve ser maior que zero",
+                new[]
+                {nameof(this.Estoque) }
+                 );
+        }
+    }
 }
+
 
 
