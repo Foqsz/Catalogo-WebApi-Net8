d44d6df2ffb1cadea63b287f49060579eb0dfb71
Victor Vinicius <victormafiavinicius@gmail.com>
2024-06-11 11:48:27 -0300

    Método para gerar um token

---
+++ WebApiCatalogo/Catalogo.Application/Interface/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace WebApiCatalogo.Catalogo.Application.Interface
+{
+    public interface ITokenService
+    {
+        JwtSecurityToken GenerateAcessToken(IEnumerable<Claim> claims, IConfiguration configuration);
+
+        string GenerateAcessToken();
+
+        ClaimsPrincipal GetPrincipalFromExperiredToken(string token, IConfiguration _config);
+    }
+}
---
+++ WebApiCatalogo/Catalogo.Application/Services/TokenService.cs
@@ -0,0 +1,49 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using WebApiCatalogo.Catalogo.Application.Interface;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApiCatalogo.Catalogo.Application.Services
+{
+    public class TokenService : ITokenService
+    {
+        public JwtSecurityToken GenerateAcessToken(IEnumerable<Claim> claims, IConfiguration _config)
+        {
+            // aqui eu obtenho o valor de SecretKey, que esta em SettingsJson
+            var key = _config.GetSection("JWT").GetValue<string>("SecretKey") ?? throw new InvalidOperationException("Invalid secret key!"); 
+
+            //aqui eu converto pra um array de bits, passando a key(secretkey)
+            var privateKey = Encoding.UTF8.GetBytes(key);
+
+            //aqui são as credenciais de assinatura, usando a SecretKey.
+            var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(privateKey), SecurityAlgorithms.HmacSha256Signature);
+
+            //aqui os valores estao vindo de settingsjson, para criar o token
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(_config.GetSection("JWT").GetValue<double>("TokenValidityInMinutes")),
+                Audience = _config.GetSection("JWT").GetValue<string>("ValidAudience"),
+                Issuer = _config.GetSection("JWT").GetValue<string>("ValidIssuer"),
+                SigningCredentials = signingCredentials
+            };
+            
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+
+            return token;
+        }
+
+        public string GenerateAcessToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExperiredToken(string token, IConfiguration _config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
