c797719bcd74ba7626908465d742e5b5a14a573c
Victor Vinicius <victormafiavinicius@gmail.com>
2024-06-12 10:28:19 -0300

    Método para criação de usuários

--- WebApiCatalogo/Catalogo.API/Controllers/AuthController.cs
+++ WebApiCatalogo/Catalogo.API/Controllers/AuthController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using WebApiCatalogo.Catalogo.Application.DTOs;
 using WebApiCatalogo.Catalogo.Application.Interface;
 using WebApiCatalogo.Catalogo.Core.Model;
 
 namespace WebApiCatalogo.Catalogo.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
@@ -22,5 +25,79 @@ namespace WebApiCatalogo.Catalogo.API.Controllers
             _roleManager = roleManager;
             _configuration = configuration;
         }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModelDTO model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName!);
+
+            if (user is not null && await _userManager.CheckPasswordAsync(user, model.Password!))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                new Claim(ClaimTypes.Name, user.UserName!),
+                new Claim(ClaimTypes.Email, user.Email!),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+
+                var token = _tokenService.GenerateAcessToken(authClaims, _configuration);
+
+                var refreshToken = _tokenService.GenerateRefreshToken();
+
+                _ = int.TryParse(_configuration["JWT:RefreshTokenValidityInMinutes"], out int refreshTokenValidityInMinutes);
+
+                user.RefreshTokenExpiryTime = DateTime.Now.AddMinutes(refreshTokenValidityInMinutes);
+
+                user.RefreshToken = refreshToken;
+
+                await _userManager.UpdateAsync(user);
+
+                return Ok(new
+                {
+                    Token = new JwtSecurityTokenHandler().WriteToken(token),
+                    RefreshToken = refreshToken,
+                    Expiration = token.ValidTo,
+                });
+            }
+            return Unauthorized();
+
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModelDTO model)
+        {
+            var userExists = await _userManager.FindByNameAsync(model.UserName!);
+
+            if (userExists != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+                 
+            }
+
+            ApplicationUser user = new()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.UserName,
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password!);
+
+            if (!result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed." });
+            }
+            return Ok(new Response { Status = "Sucess", Message = "User created sucessefully!" });
+        }
+
     }
 }
