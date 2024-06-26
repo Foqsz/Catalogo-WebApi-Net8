5a389496aae4aba9d7b2b6700b1e59a8b9e5898a
Victor Vinicius <victormafiavinicius@gmail.com>
2024-06-26 10:10:28 -0300

    Testes de unidade

---
+++ WebApiCatalogoxUnitTests/Unit Tests/GetProdutoUnitTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiCatalogo.Catalogo.API.Controllers;
+using WebApiCatalogoxUnitTests.UnitTests;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
+
+namespace WebApiCatalogoxUnitTests.Unit_Tests
+{
+    public class GetProdutoUnitTests : IClassFixture<ProdutosUnitTestController>
+    {
+        private readonly ProdutosController _controller;
+
+        public GetProdutoUnitTests(ProdutosUnitTestController controller)
+        {
+            _controller = new ProdutosController((ILogger<ProdutosController>)controller.logger, controller.repository, controller.mapper);
+        }
+
+        [Fact]
+        public async Task GetProdutoById_OkResult()
+        {
+            //Arrange
+            var produtoId = 2;
+
+            //Act
+            var data = await _controller.Get(produtoId);
+
+            //Assert(xunit)
+            //var okResult = Assert.IsType<OkObjectResult>(data.Result);
+           // Assert.Equal(200, okResult.StatusCode);
+
+            //assert (fluentAssertions)
+            data.Result.Should().BeOfType<OkObjectResult>()//verifica se o resultado é do tipo okobjectresult
+                .Which.StatusCode.Should().Be(200); //verifica se o codigo de status do okobjectresult é 200
+
+        }
+    }
+}
---
+++ WebApiCatalogoxUnitTests/Unit Tests/ProdutosUnitTestController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiCatalogo.Catalogo.Application.DTOs.Mappings;
+using WebApiCatalogo.Catalogo.Infrastucture.Context;
+using WebApiCatalogo.Catalogo.Infrastucture.Repository;
+
+namespace WebApiCatalogoxUnitTests.UnitTests;
+
+public class ProdutosUnitTestController
+{
+    public IUnitOfWork repository;
+    public IMapper mapper;
+    public ILogger logger;
+    public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+
+    public static string connectionString = "Server=localhost;DataBase=apicatalogodb;Uid=root;Pwd=Hw8vup5e;";
+
+    static ProdutosUnitTestController()
+    {
+        dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+            .Options;
+    }
+
+    public ProdutosUnitTestController()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new ProdutoDTOMappingProfile());
+        });
+
+        mapper = config.CreateMapper();
+        var context = new AppDbContext(dbContextOptions);
+        repository = new UnitOfWork(context);
+    }
+}
